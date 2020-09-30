ed3130ec970abd484f2d9e784571c5ce94c24ca0
Jules Gagnaire <jules.gagnaire@protonmail.com>
2020-09-30 14:14:15 +0200

    responsive

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -4,6 +4,11 @@ import ContactForm from "../components/ContactForm";
 export const Contact = () => (
   <div className="contact-page">
     <h1>Contactez-nous</h1>
+    <p>
+      Des questions, des suggestions, contactez-nous ! Vous pouvez nous écrire
+      directement à parents (at) collectberanger.paris ou directement via le
+      formulaire ci-dessous.
+    </p>
     <ContactForm />
   </div>
 );
--- src/containers/Parents.js
+++ src/containers/Parents.js
@@ -2,4 +2,9 @@ import React from "react";
 
 import { Masonry } from "../components";
 
-export const Parents = () => <Masonry />;
+export const Parents = () => (
+  <div className="parents-page">
+    <h1>Les parents</h1>
+    <Masonry />
+  </div>
+);
